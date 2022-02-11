83f9bbdceade040ca29991388ffa9eefb49e51d4
AngelPiva <angelpiva6@gmail.com>
2022-02-11 10:18:56 -0800

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers using the Bubble sort algorithm
+ * @array: array
+ * @size: size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t n = 1, i = 0;
+	int tmp = 0;
+
+	while (n == 1)
+	{
+		n = 0;
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				n = 1;
+				print_array(array, size);
+			}
+		}
+	}
+}
