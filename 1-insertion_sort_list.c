a53c9b53fd992497a3427d6c19d3a7f44883c1bd
AngelPiva <angelpiva6@gmail.com>
2022-02-14 08:30:10 -0800

    task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,7 +9,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *tmp = NULL, *aux = NULL;
 
-	if (!(*list) || !list || !(*list)->next)
+	if (*list == NULL || list == NULL || (*list)->next == NULL)
 		return;
 
 	tmp = *list;
